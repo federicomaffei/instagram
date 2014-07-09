55ffe87977d23da945034c9fb6c3c7e345e52bff
federicomaffei <federico.maffei@gmail.com>
2014-07-09 13:07:31 +0100

    changed aws setup

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 gem 'paperclip', github: 'thoughtbot/paperclip'
 
+gem 'aws-sdk'
+
 # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
 gem 'spring',        group: :development
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ActiveRecord::Base
 
-	has_attached_file :image, :styles => { :medium => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "100x100>" }, storage: :s3,
+  		:s3_credentials => {
+    		bucket: 'pizzagram',
+    		access_key_id: Rails.application.secrets.s3_access_key,
+    		secret_access_key: Rails.application.secrets.s3_secret_key
+    	}
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
+ 
