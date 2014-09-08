ff85b7016613f2f76c8e4948b6d270cf6f4fa11e
federicomaffei <federico.maffei@gmail.com>
2014-09-08 12:24:13 +0100

    enhanced Likes controller and model such as it allows the User only to like a post once.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
  
 	def create
 		@post = Post.find_by_title(params[:post_id].gsub("_", " "))
-		@post.likes.create!
+		Like.create(post:@post, user:current_user )
 		WebsocketRails[:likes].trigger 'new', { id: @post.id, new_like_count: @post.likes.count}
 		redirect_to '/posts'
 	rescue
